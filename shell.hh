1da5b6cd1d387ed6cae4042c38d9e71f35f8dc27
Nathaniel Johnson <john2791@purdue.edu>
2022-09-07 11:43:56 -0400

    Delete shell.hh

--- shell.hh
+++
@@ -1,9 +0,0 @@
-#ifndef shell_hh
-#define shell_hh
-#include "command.hh"
-
-struct Shell {
-        std::vector<SimpleCommand *> historyTable = std::vector<SimpleCommand *>();
-        static void prompt();
-        static Command currentCommand;
-};
